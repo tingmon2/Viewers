b59c9e7c696a6f48d8c1ca1b74829302d18058da
tingmon2 <tingmon2@gmail.com>
2023-10-10 15:01:43 +0900

    일단 접속은 됨
    http://43.200.164.237:8080
    
    http://testohif-orthanc-cors.s3-website.ap-northeast-2.amazonaws.com

--- platform/app/public/config/docker_nginx-orthanc.js
+++ platform/app/public/config/docker_nginx-orthanc.js
@@ -16,9 +16,9 @@ window.config = {
       configuration: {
         friendlyName: 'Orthanc Server',
         name: 'Orthanc',
-        wadoUriRoot: 'http://43.200.164.237:8042/dicom-web',
-        qidoRoot: 'http://43.200.164.237:8042/dicom-web',
-        wadoRoot: 'http://43.200.164.237:8042/dicom-web',
+        wadoUriRoot: 'http://43.200.164.237:8080/dicom-web',
+        qidoRoot: 'http://43.200.164.237:8080/dicom-web',
+        wadoRoot: 'http://43.200.164.237:8080/dicom-web',
         qidoSupportsIncludeField: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
